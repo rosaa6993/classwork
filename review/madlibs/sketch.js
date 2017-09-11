89a84a95a3048ca8f3ff8cac0ff62c9c5832d363
Amber L Rosa <amberl.rosa@f1006-18-34184.bmcc.cuny.edu>
2017-09-11 17:09:48 -0400

    add review

---
+++ review/madlibs/sketch.js
@@ -0,0 +1,10 @@
+    alert("Click ok to start.");
+      var name = prompt("Give me a name please");
+      name = name.toUpperCase();
+      var thing =  prompt("now an object");
+      var activity = prompt("now an action ending with -ing");
+      var description  = prompt("now a descriptive word");
+      var place = prompt ("and finally a location ");
+      alert("Please click OK for the Result");
+      var FinalSentence = name + " has a " + thing + " on " + " their head and they're are " + activity +  " in a " + description + " way" + " in " + place;
+      
